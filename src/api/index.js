e1ccdb9492418c37cf68d56af8cac0cb74037689
Ashfaque <nvjkvhdjkh@gmail.com>
2021-12-16 19:44:18 +0600

    second commit

--- src/api/index.js
+++ src/api/index.js
@@ -22,35 +22,17 @@ export const fetchData = async () => {
 
 // export const fetchDailyData = async () => {
 //   try {
-//     const { data } = await axios.get(`${url}/daily`);
+//     const { data } = await axios.get(
+//       "https://api.covidtracking.com/v1/us/daily.json"
+//     );
 
-//     const modifiedData = data.map((dailyData) => ({
-//       confirmed: dailyData.confirmed.total,
-//       deaths: dailyData.deaths.total,
-//       date: dailyData.reportDate,
+//     return data.map(({ positive, recovered, death, dateChecked: date }) => ({
+//       confirmed: positive,
+//       recovered,
+//       deaths: death,
+//       date,
 //     }));
-
-//     return modifiedData;
 //   } catch (error) {
 //     return error;
 //   }
 // };
-
-export const fetchDailyData = async () => {
-  try {
-    const {
-      data: { positive, recovered, death, dateChecked },
-    } = await axios.get("https://api.covidtracking.com/v1/us/daily.json");
-
-    const modifiedData = {
-      positive,
-      recovered,
-      death,
-      dateChecked,
-    };
-    console.log(modifiedData);
-    return modifiedData;
-  } catch (error) {
-    return error;
-  }
-};
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,54 +1,129 @@
-import React, { useState, useEffect } from "react";
-import { Line, Bar } from "react-chartjs-2";
-
-import { fetchDailyData } from "../../api";
-
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 import styles from "./Chart.module.css";
 
-const Chart = ({ data: { positive, recovered, death, dateChecked } }) => {
-  const [dailyData, setDailyData] = useState({});
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-  useEffect(() => {
-    const fetchAPI = async () => {
-      const dataDaily = await fetchDailyData();
-      setDailyData(dataDaily);
-    };
-
-    fetchAPI();
-  }, [setDailyData]);
-
-  const lineChart = dailyData[0] ? (
-    <Line
-      data={{
-        labels: dailyData.map(({ dateChecked }) =>
-          new Date(dateChecked).toLocaleDateString()
-        ),
-        datasets: [
-          {
-            data: dailyData.map((data) => data.positive),
-            label: "Infected",
-            borderColor: "#3333ff",
-            fill: true,
-          },
-          {
-            data: dailyData.map((data) => data.death),
-            label: "Deaths",
-            borderColor: "red",
-            backgroundColor: "rgba(255, 0, 0, 0.5)",
-            fill: true,
-          },
-          {
-            data: dailyData.map((data) => data.recovered),
-            label: "Recovered",
-            borderColor: "green",
-            backgroundColor: "rgba(0, 255, 0, 0.5)",
-            fill: true,
-          },
-        ],
-      }}
-    />
-  ) : null;
-
-  return <div className={styles.container}>{lineChart}</div>;
+const Chart = () => {
+  return (
+    <div className={styles.container}>
+      <Line
+        data={{
+          labels: [
+            "January",
+            "February",
+            "March",
+            "Äpril",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ],
+          datasets: [
+            {
+              label: "My first datasets",
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              borderCapStyle: "butt",
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: "miter",
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: [65, 59, 80, 81, 56, 55, 40],
+            },
+          ],
+        }}
+      />
+    </div>
+  );
 };
+
 export default Chart;
+
+// import React, { useState, useEffect } from "react";
+// import { fetchDailyData } from "../../api";
+// import { Line, Bar } from "react-chartjs-2";
+// import styles from "./Chart.module.css";
+
+// const Chart = () => {
+//   const [dailyData, setDailyData] = useState([]);
+
+//   useEffect(() => {
+//     const fetchAPI = async () => {
+//       const fetchedData = await fetchDailyData();
+//       setDailyData(fetchedData);
+//     };
+//     fetchAPI();
+//   }, [setDailyData]);
+
+//   const lineChart = dailyData[0] ? (
+//     <Line
+//       data={{
+//         labels: dailyData.map(({ dateChecked }) => dateChecked),
+//         datasets: [
+//           {
+//             data: dailyData.map(({ positive }) => positive),
+//             label: "Infected",
+//             borderColor: "#3333ff",
+//             fill: true,
+//           },
+//           {
+//             data: dailyData.map(({ death }) => death),
+//             label: "Infected",
+//             borderColor: "red",
+//             backgroundColor: "rgba(255,0,0,0.5)",
+//             fill: true,
+//           },
+//         ],
+//       }}
+//     />
+//   ) : null;
+//   return (
+//     <>
+//       <h1>Chart</h1>
+//       {dailyData.map((data) => {
+//         return <h1>{data.dateChecked}</h1>;
+//       })}
+//     </>
+//   );
+// };
+// export default Chart;
+
+// // .map(({ positive, recovered, death, dateChecked: date }) => ({
+// //   confirmed: positive,
+// //   recovered,
+// //   deaths: death,
+// //   date,
+// // }));
