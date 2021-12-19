5aa933eed6c271d722c00b7ef9f1103b9d3922e4
Ashfaque <nvjkvhdjkh@gmail.com>
2021-12-19 18:46:28 +0600

    fifth commit

--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -17,10 +17,35 @@ const CountryPicker = () => {
     fetchAPI();
   }, [setCountries]);
 
+  const formatDate = (date) => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = `0${d.getMonth() + 1}`.slice(-2);
+    const _date = d.getDate();
+    return `${year}-${month}-${_date}`;
+  };
+
   const handleCountryChange = (e) => {
-    console.log(e.target.value);
-    //fetch the data
-    // set the state
+    const d = new Date();
+    const to = formatDate(d);
+    const from = formatDate(d.setDate(d.getDate() - 6));
+    // console.log(from, to);
+    coronaReportByDateRange(e.target.value, from, to);
+  };
+
+  const coronaReportByDateRange = async (countrySlug, from, to) => {
+    try {
+      const res = await axios.get(
+        `https://api.covid19api.com/country/${countrySlug}/status/confirmed?from=${from}T00:00:00Z&to=${to}T00:00:00Z`
+      );
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const daysHandler = (e) => {
+    setDays(e.target.value);
   };
   return (
     <>
@@ -35,7 +60,7 @@ const CountryPicker = () => {
         </NativeSelect>
       </FormControl>
       <FormControl>
-        <NativeSelect value={days}>
+        <NativeSelect value={days} onChange={daysHandler}>
           <option value="7">Last 7 days</option>
           <option value="30">Last 30 days</option>
           <option value="90">Last 90 days</option>
