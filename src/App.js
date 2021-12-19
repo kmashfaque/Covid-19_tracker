d79a3abfe5c103695fc9edeb0514350c94d58256
Ashfaque <nvjkvhdjkh@gmail.com>
2021-12-19 17:36:39 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import CountryPicker from "./components/CountryPicker/CountryPicker";
 import styles from "./App.module.css";
 
 class App extends React.Component {
+  state = {
+    country: "",
+  };
+
+  handleCountryChange = async (country) => {
+    console.log(country);
+    //fetch the data
+    // set the state
+  };
   render() {
     return (
       <div className={styles.container}>
         <Cards />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
       </div>
     );
--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import styles from "./CountryPicker.module.css";
+import axios from "axios";
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const url = "https://api.covid19api.com/summary";
+      const { data } = await axios.get(url);
+      setCountries(data.Countries);
+    };
+
+    fetchAPI();
+  }, [setCountries]);
   return (
-    <>
-      <h2>CountryPicker</h2>
-    </>
+    <FormControl>
+      <NativeSelect
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value="global">Global</option>
+        {countries.map((country) => (
+          <option value={country.Slug} key={country.ID}>
+            {country.Slug}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
   );
 };
 
